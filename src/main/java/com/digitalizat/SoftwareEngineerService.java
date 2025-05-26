7b9d869b442cbefcda993a7a07bbcdc1df516864
Hugo Martínez Barros <hugo.martinez.ba2003@gmail.com>
2025-05-26 18:02:41 +0200

    refactor: simplify SoftwareEngineerService by using Lombok and improving exception handling

--- src/main/java/com/digitalizat/SoftwareEngineerService.java
+++ src/main/java/com/digitalizat/SoftwareEngineerService.java
@@ -1,45 +1,40 @@
 package com.digitalizat;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class SoftwareEngineerService {
-    private SoftwareEngineerRepository softwareEngineerRepository;
 
-    public SoftwareEngineerService(SoftwareEngineerRepository softwareEngineerRepository) {
-        this.softwareEngineerRepository = softwareEngineerRepository;
-    }
+    private final SoftwareEngineerRepository softwareEngineerRepository;
 
-    // Gets all Software Engineers from the database
     public List<SoftwareEngineer> getAllSoftwareEngineers() {
         return softwareEngineerRepository.findAll();
     }
 
-    // Retrieves a Software Engineer by ID, throws an exception if not found
     public SoftwareEngineer getSoftwareEngineerById(Integer id) {
         return softwareEngineerRepository.findById(id)
-            .orElseThrow(() -> new org.springframework.web.server.ResponseStatusException(
-                org.springframework.http.HttpStatus.NOT_FOUND,
-                "Software Engineer with ID " + id + " does not exist."
-            ));
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Software Engineer with ID " + id + " does not exist."
+                ));
     }
 
-    // Adds a new Software Engineer to the database
     public void createSoftwareEngineer(SoftwareEngineer softwareEngineer) {
         softwareEngineerRepository.save(softwareEngineer);
     }
 
-    // Deletes a Software Engineer by ID from the database
     public void deleteSoftwareEngineerById(Integer id) {
         softwareEngineerRepository.deleteById(id);
     }
 
-    // Updates an existing Software Engineer in the database
     public void updateSoftwareEngineer(SoftwareEngineer softwareEngineer) {
         getSoftwareEngineerById(softwareEngineer.getId());
         softwareEngineerRepository.save(softwareEngineer);
     }
-
 }
